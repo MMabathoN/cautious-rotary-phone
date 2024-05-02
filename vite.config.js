8443aec658cf3e33bc14444d8f2981107a75622d
MMabathoN <161308587+MMabathoN@users.noreply.github.com>
2024-05-02 13:49:32 +0000

    APP.JSX RESTRUCTURE

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js or vite.config.ts
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css']
+    }
+  }
+});
